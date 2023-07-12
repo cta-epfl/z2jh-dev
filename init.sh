024d7f932776786cfe4e5923140f78d9dac6cf72
Bastien Wermeille <bastien.wermeille@gmail.com>
2023-07-12 16:04:27 +0200

    Multi OAuth provider system

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@
 git clone git@github.com:cta-epfl/downloadservice.git hub_image/downloadservice
 git clone git@github.com:cta-epfl/ctadata.git user_image/ctadata
 
-minikube image build -t k8s-hub:latest ./hub_image
-minikube image build -t minimal-notebook:latest ./user_image
+minikube image build -t k8s-hub:0.0.1-dev ./hub_image
+minikube image build -t datascience-notebook:0.0.1-dev ./user_image
